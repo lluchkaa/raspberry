6c7e01a0c9c04856b52cdb4743312cb959701c8a
Yura Luchka <lluchkaa@gmail.com>
2026-02-03 22:45:07 +0200

    update wireless

--- nix/os/networking.nix
+++ nix/os/networking.nix
@@ -4,17 +4,38 @@
     hostName = "raspberry";
 
     # Static IP configuration
-    interfaces.end0 = {
-      useDHCP = false;
-      ipv4.addresses = [
-        {
-          address = "192.168.1.100";
-          prefixLength = 24;
-        }
-      ];
+    interfaces = {
+      end0 = {
+        useDHCP = false;
+        ipv4.addresses = [
+          {
+            address = "192.168.0.103";
+            prefixLength = 24;
+          }
+        ];
+      };
+      wlan0 = {
+        useDHCP = false;
+        ipv4.addresses = [
+          {
+            address = "192.168.0.104";
+            prefixLength = 24;
+          }
+        ];
+      };
     };
 
-    defaultGateway = "192.168.1.1";
+    wireless = {
+      enable = true;
+      environmentFile = "/run/secrets/wireless-env";
+      networks = {
+        "@WIFI_SSID@" = {
+          psk = "@WIFI_PSK@";
+        };
+      };
+    };
+
+    defaultGateway = "192.168.0.1";
     nameservers = [
       "1.1.1.1"
       "8.8.8.8"
