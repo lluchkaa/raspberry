bbfc3129c0f8587eac2627c08b0671ee843cc48f
Yura Luchka <lluchkaa@gmail.com>
2026-02-04 22:56:59 +0200

    update

--- nix/os/user.nix
+++ nix/os/user.nix
@@ -19,5 +19,8 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOKn3uiYhv6fwhtnM6sv11jI4KQGAKAWXEMnRPfbaTvY lluchkaa@gmail.com"
     ];
+
+    # Default password: admin (change after first login!)
+    initialHashedPassword = "$6$/LxBfBCrnDVzH9B6$oIc7BRPyP4NmNuuPlDDuISlBMJ5qytiAQpT8y9V515koCV4J4jo4HbRk8lU5pNfDDFXkIt9kCg8ySsXkWIahw.";
   };
 }
